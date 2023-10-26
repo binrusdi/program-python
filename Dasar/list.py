7706cd9edfce642b58bbfd3bf642fe3359de0c0f
binrusdi <mrusdiana07@gmail.com>
2023-10-26 17:45:18 +0700

    list

---
+++ Dasar/list.py
@@ -0,0 +1,8 @@
+"""Memamhami List lebih dalam"""
+
+no_urut = [0, 1, 2, 3]  # Ini adalah list
+
+# Cara mengakses list
+
+print(no_urut[2])
+# irisan dalam LIST berbeda dengan irisan STRING
--- Dasar/string.py
+++ Dasar/string.py
@@ -3,12 +3,12 @@
 str_kutip_satu = 'String bisa diberikan dengan TANDA KUTIP SATU (\')'
 print(str_kutip_satu)
 
-print("\n") # Hiraukan ini jika anda melihat di code editor
+print("\n")  # Hiraukan ini jika anda melihat di code editor
 
 str_kutip_dua = "String juga bisa diberikan dengan TANDA KUTIP DUA (\")"
 print(str_kutip_dua)
 
-print("\n") # Hiraukan ini jika anda melihat di code editor
+print("\n")  # Hiraukan ini jika anda melihat di code editor
 
 str_kutip_tiga = """
 String juga bisa diberikan dengan TANDA KUTIP TIGA (\"\"\")
@@ -20,11 +20,15 @@ print(str_kutip_tiga)
 karakter = "karakter"
 print(f"Jumlah karakter menggunakan len() yaitu {len(karakter)}")
 
-print("\n") # Hiraukan ini jika anda melihat di code editor
+print("\n")  # Hiraukan ini jika anda melihat di code editor
 
 # Cara menghitung index string pada python yaitu, setiap karakter dibungkus oleh sebuah pembatas kiri kanan, pembatas kiri di awal karakter itu index 0, sedangkan pembatas setelah karakter itu index 1. Jika hanya satu karakter.
-
-print(karakter[0:]) # mengambil hurup pertama sampai akhir
-print(karakter[:4]) # mengambil karakter sampai index ke 4
-
-
+"""
+ +---+---+---+---+---+---+
+ | P | y | t | h | o | n |
+ +---+---+---+---+---+---+
+ 0   1   2   3   4   5   6
+-6  -5  -4  -3  -2  -1
+"""
+print(karakter[0:])  # mengambil hurup pertama sampai akhir
+print(karakter[:4])  # mengambil karakter sampai index ke 4
