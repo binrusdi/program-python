8193f2b41a7603ca22ea3bba459674f20e3c2758
binrusdi <mrusdiana07@gmai.com>
2023-10-27 22:17:25 +0700

    modief

--- Dasar/control_flow_tools.py
+++ Dasar/control_flow_tools.py
@@ -1,1 +1,81 @@
+# While
+a, b = 0, 1
+while a < 10:
+    print(a)
+    a, b = b, a + b
+"""
+    1, 1  = 1, 0 + 1
+    1, 2  = 1, 1 + 1
+    2, 3  = 2, 1 + 2
+    3, 5  = 3, 2 + 3
+    5, 8  = 5, 3 + 5
+    8, 13 = 8, 5 + 8
+
+    cara menghitung pibonanci di belakang layar
+"""
+# => Argumen kata kunci end didalam method print() menghindari baris baru pada output, atau mengakhiri output dengan sting yang berbeda
+c, d = 0, 1
+while a < 1000:
+    print(a, end=",")
+    a, b = b, a + b
+
+
+
 # Pernyataan IF
+print("\n")
+"""
+nilai = int(input("Please input nilai dengan integer/angka : "))
+
+if nilai >= 70:
+    print("Nilai nya terlalu besar")
+elif nilai <= 20:
+    print("Nilai terlalu kecil")
+elif nilai == 50:
+    print("Nilai nya pas dengan yang diharapkan")
+else:
+    print("Tidak mendekati")
+"""
+
+# Pernyataan FOR
+# for pada python berbeda dengan bahasa programan lain
+
+# iterasi list dan mendapatkan panjang dari karakter, item pada list
+hewans = ["Kucing", "Singa", "Meong"]
+for hewan in hewans:
+    print(hewan, len(hewan))
+
+# Mengulang salinan koleksi sambil mengubahnya
+users = {
+    "hans" : "active",
+    "jhons" : "inactive",
+    "melly" : "active"
+}
+# mengulangi salinan
+for user, status in users.copy().items():
+    if status == 'inactive':
+        del users[user]
+# buat koleksi baru
+active_users = {}
+for user, status in users.items():
+    if status == 'active':
+        active_users[user] = status
+print(active_users)
+
+""" FUNGSI """
+
+# Mengulangi serangkaian angka dengan range()
+# Rumus -> range(start, rentang, kenaikan)
+# endpoit tidak pernah dilibatkan dalam serangkaian iterasi
+for i in range(5):
+    print(i)
+
+range(5, 10) # jangan digunakan
+r = range(5, 10) # jangan digunakan
+
+# Membuat list dengan range
+lr = list(range(2, 10))
+print(lr)
+
+# Dengan menambahkan aturan kenaikan
+ll = list(range(3, 15, 5))
+print(ll)
--- Dasar/string.py
+++ Dasar/string.py
@@ -18,7 +18,7 @@ print(str_kutip_tiga)
 """ String dalam python dapat di index, urutan pertama adalah 0 """
 
 karakter = "karakter"
-print(f"Jumlah karakter menggunakan len() yaitu {len(karakter)}")
+print(f"Menampilkan jumlah karakter menggunakan len()\n dimana variable karakter = \"karakter\" adalah berjumlah {len(karakter)}")
 
 print("\n")  # Hiraukan ini jika anda melihat di code editor
 
@@ -32,3 +32,34 @@ print("\n")  # Hiraukan ini jika anda melihat di code editor
 """
 print(karakter[0:])  # mengambil hurup pertama sampai akhir
 print(karakter[:4])  # mengambil karakter sampai index ke 4
+print('\n')
+# Keunikan string menggunakan IS yang mengembalikan boolean, tapi tanda baca itu menghasilkan false
+
+a = 'linux'
+b = 'linux'
+
+print( 'a = \' linux \'' )
+print( 'b = \' linux \'' )
+
+print( 'a is b' )
+print(a is b)
+
+# Escape di python
+"""
+\newline	Ignored
+\\	Backslash (\)
+\'	Single quote (')
+\"	Double quote (")
+\a	ASCII Bell (BEL)
+\b	ASCII Backspace(BS)
+\f	ASCII Formfeed (FF)
+\n	ASCII Linefeed (LF)
+\N{name}	Character named name in the Unicode database (Unicode only)
+\r	ASCII Carriage Return (CR)
+\t	ASCII Horizontal Tab (TAB)
+\uxxxx	Character with 16-bit hex value xxxx (Unicode only)
+\Uxxxxxxxx	Character with 32-bit hex value xxxxxxxx (Unicode only)
+\v	ASCII Vertical Tab (VT)
+\ooo	Character with octal value ooo
+\xhh	Character with hex value hh
+"""
