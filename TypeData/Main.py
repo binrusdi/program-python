e350c3e2bd4faa08e7503ee8d4082c2701015c77
binrusdi <mrusdiana07@gmail.com>
2023-11-03 09:12:52 +0700

    rubah

---
+++ TypeData/Main.py
@@ -0,0 +1,30 @@
+''' Type data bawaan python '''
+'''
+Jenis teks => str
+Jenis numerik => int, float, complex
+Jenis urutan => list, tuple, range
+Jenis pemetaan => dict
+Jenis set => set, frozenset
+Jenis boolean => bool
+Jenis biner => bytes, bytearray, memoryview
+Tidak ada jenis => typeNone
+'''
+
+''' Mengatur type data '''
+'''
+x = 'Hello world' # str
+x = 20 # int
+x = 20.5 # float
+x = 1j # complex
+x = ['pisang', 'semangka', 'anggur'] # list
+x = ('sirsak', 'durian', 'melon') # tuple
+x = range(6) # range
+x = {'nama': 'Rusdiana', 'age': 29} # dict
+x = {'apple', 'banana', 'cherry'} # set
+x = ({'apple', 'banana', 'cherry'}) # frozenset
+x = True # bool
+x = b'Hello' # bytes
+x = bytearray(5) # bytearray
+x =  memoryview(bytes(5)) # memoryview
+x = None # NoneType
+'''
