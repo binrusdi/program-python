a3fad341c774dab700bc9e03f9e0d6554fea50e1
binrusdi <mrusdiana07@gmai.com>
2023-11-09 17:22:31 +0700

    update lagi

--- 07_DictPy/Main.py
+++ 07_DictPy/Main.py
@@ -24,3 +24,32 @@ print(myCar)
 # Menentukan berapa banyak item yang dimiliki dict, gunakan fungsi len().
 print(len(myCar))
 
+# Menampilkan tipe data, gunakan fungsi type().
+print(type(myCar))
+
+# Membuat ditc dengan konstruktor dict().
+myfruits = dict(name='manggo', price=12000, expired='2023')
+print(myfruits)
+
+# Akses item pada dict menggunakan key
+fruit = myfruits['name']
+print(fruit)
+
+# Juga dapat diakses dengan get()
+x = myfruits.get('price')
+print(x)
+
+# Mendapatkan key yang ada pada dict, gunakan keys()
+# Daftar key pada dict, artinya setiap perubahaan yang dilakukan pada kamus akan tercermin pada dalam daftar key
+key_dict = myfruits.keys()
+print(key_dict)
+
+# Mendapatkan value dengan menggunakan values()
+# Dengan ini, values() menampilkan semua dafta value yang ada pada dict
+value_dict = myfruits.values()
+print(value_dict)
+
+# Mendapatkan semua item dari dict menggunakan items()
+# items() akan mengembalikan item dict sebagai tuple dalam daftar
+type_tuple = myfruits.items()
+print(f'{type_tuple}, ini adalah {type(type_tuple)}')
