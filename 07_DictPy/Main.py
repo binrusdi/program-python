183e55d0d069d40cc20ad2cb49d9e7fc6dcf760a
binrusdi <mrusdiana07@gmai.com>
2023-11-11 02:12:13 +0700

    update dict

--- 07_DictPy/Main.py
+++ 07_DictPy/Main.py
@@ -53,3 +53,94 @@ print(value_dict)
 # items() akan mengembalikan item dict sebagai tuple dalam daftar
 type_tuple = myfruits.items()
 print(f'{type_tuple}, ini adalah {type(type_tuple)}')
+
+# Memeriksa key, apakah ada di dalam dict menggunakan in
+if 'name' in myfruits:
+    print('yes, "name" is one keys in the dictionaries')
+
+# Anda dapat mengubah nilai item dengan mengacu pada nama key nya.
+biodata = {
+    'nama': 'Rusdiana',
+    'alamat': 'Cihideung',
+    'tanggal_lahir': '28 Mei 1994',
+}
+biodata['alamat'] = 'Lembur Sawah'
+print(biodata)
+
+# Memperbaharui dict dengan menggunakan update()
+# Method ini akan meperbaharui kamus dengan item dari argumen yang diberikan
+# Argumen nya harus berupa dict atau object yang dapat diubah dengan pasangan key:value
+alamat = {
+    'jalan': 'pangalengan',
+    'desa': 'tribaktimulya',
+    'kec': 'pangalengan',
+}
+alamat.update({'jalan': 'pangalengan-banjaran'})
+print(alamat)
+
+# Menambhakan item pada dict
+alamat['rt'] = '04'
+alamat['rw'] = '06'
+print(alamat)
+
+# Menghapus dengan beberapa method
+# pop() -> menghapus dengan item yang ditentukan
+alamat.pop('jalan')
+print(alamat)
+# popitem() -> menghapus item terakhir yang dimasukan
+alamat.popitem()
+print(alamat)
+# clear() -> mengosongkan item saja
+alamat.clear()
+print(alamat)
+'''
+# del -> menghapus item dengan nama key yang di tentukan, juga dapat menghapus seluruh dict
+del alamat['kec']
+print(alamat)
+del alamat
+print(alamat) # NameError: name 'alamat' is not defined karna dict nya sudah tidak ada
+'''
+# Mengiterasi dict dengan for
+karyawan = {
+    'pria': 'angga',
+    'perempuan': 'citra',
+}
+for kunci, nilai in karyawan.items():
+    print(f'{kunci} diwakili oleh {nilai}')
+
+# Menyalin dict dengan method copy()
+salinan_dict = karyawan.copy()
+print(salinan_dict)
+
+# Cara lain untuk menyalin dict dengan menggunakan, construktor dict()
+salin_again = dict(salinan_dict)
+print(salin_again)
+
+# Dict bersarang
+# cara ke -1
+myfamily = {
+    'child1': {
+        'name': 'Ahmda',
+        'year': '2023'
+    },
+    'child2': {
+        'name': 'Agus',
+        'year': '2021'
+    }
+}
+print(myfamily)
+# cara ke -2
+toyota = {
+    'model': 'Avanza',
+    'year': '2019'
+}
+honda = {
+    'model': 'yaris',
+    'year': '2020'
+}
+mobil = {
+    'merek_1': toyota,
+    'merek_2': honda
+}
+
+print(mobil)
