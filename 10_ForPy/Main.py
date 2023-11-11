d170224cfe24f3f2d92c18cbe1b6f92c2262883f
binrusdi <mrusdiana07@gmail.com>
2023-11-11 20:42:05 +0700

    forpy

---
+++ 10_ForPy/Main.py
@@ -0,0 +1,10 @@
+''' For Python '''
+'''
+Perulangan for digunakan untuk mengulangi suatu urutan(list, tuple, dict, set,
+dan string)
+'''
+
+# contoh
+nama = "rusdiana"
+for chart in nama:
+  print(chart)
