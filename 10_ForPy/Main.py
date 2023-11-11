70fb677a3745cfa1b99d16f8d9c95068c1196d81
binrusdi <mrusdiana07@gmail.com>
2023-11-11 23:07:22 +0700

    loop

--- 10_ForPy/Main.py
+++ 10_ForPy/Main.py
@@ -4,7 +4,25 @@ Perulangan for digunakan untuk mengulangi suatu urutan(list, tuple, dict, set,
 dan string)
 '''
 
-# contoh
+# String
 nama = "rusdiana"
 for chart in nama:
   print(chart)
+  
+# list
+fruits = ['pisang', 'cherry', 'apel']
+for fruit in fruits:
+  print(fruit)
+  
+# for bisa di tambahkan dengan break atau continue
+# catatan 0enting tentang break
+mobil = ['alpart', 'toyota', 'yaris']
+for x in mobil:
+  if x == 'toyota':
+    break # toyota tidak akan dicetak
+  print(x)
+
+for y in mobil:
+  print(y)
+  if y == 'toyota':
+    break # toyota akan dicetak
